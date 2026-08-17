d5c93428154f3155eb649d9e62d573b6fbe1e723
HARINI S <harinisv6@gmail.com>
2026-08-17 17:09:07 +0530

    Create main.py

---
+++ main/main.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+
+@app.get("/")
+def home():
+    return FileResponse("index.html")
+
+
+@app.get("/health")
+def health():
+    return {"status": "Hindu Smart Clock API is running"}
